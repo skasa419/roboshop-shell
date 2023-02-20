88a2ba4f3960500f69b39261bd3a09cd1f899a9d
srikanth <ks.srikanth121@gmail.com>
2023-02-20 17:53:21 +0530

    frontend and mongodb

--- frontend.sh
+++ frontend.sh
@@ -3,6 +3,6 @@ rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-cp configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
+cp configs/ngnx-roboshop.conf /etc/nginx/default.d/roboshop.conf
 systemctl enable nginx
 systemctl restart nginx
