3394a3ad8456ffed85f199f602a47e3f4a3e1e0b
srikanth <ks.srikanth121@gmail.com>
2023-02-20 19:03:00 +0530

    frontend and mongodb

--- frontend.sh
+++ frontend.sh
@@ -1,3 +1,4 @@
+code_dir=$(pwd)
 echo -e "\e[35mInstalling nginx\e[0m"
 yum install nginx -y
 echo -e "\e[35mRemoving Old Conent\e[0m"
@@ -8,7 +9,7 @@ echo -e "\e[35mExtracting Download Frontend\e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 echo -e "\e[35mCopying Nginx Config for Roboshop\e[0m"
-cp configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
+cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
 echo -e "\e[35mEnabling Nginx\e[0m"
 systemctl enable nginx
 echo -e "\e[35mStarting Nginx\e[0m"
