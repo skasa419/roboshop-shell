43b44d38611809099c63c20704fcb9299aed066b
srikanth <ks.srikanth121@gmail.com>
2023-02-22 17:52:58 +0530

    user

--- common.sh
+++ common.sh
@@ -15,3 +15,73 @@ status_check() {
     exit 1
   fi
 }
+
+NODEJS(){
+print_head "Configure nodejs Repository"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
+status_check $?
+
+print_head "Install Nodejs"
+yum install nodejs -y &>>${log_file}
+status_check $?
+
+
+print_head "Add roboshop user"
+id roboshop &>>${log_file}
+if [ $? -ne 0 ]; then
+  useradd roboshop &>>${log_file}
+fi
+status_check $?
+
+print_head "Create Application Directory"
+if [ ! -d /app ]; then
+  mkdir /app &>>${log_file}
+fi
+status_check $?
+
+print_head "Delete Old Content"
+rm -rf /app/* &>>${log_file}
+status_check $?
+
+print_head "Download App Content"
+curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log_file}
+status_check $?
+cd /app
+
+print_head "Extracting App Content"
+unzip /tmp/${component}.zip &>>${log_file}
+status_check $?
+
+print_head "Installing Nodejs Dependencies"
+npm install &>>${log_file}
+status_check $?
+
+print_head "Copying SystemD service File"
+cp ${code_dir}/configs/${component}.service /etc/systemd/system/${component}.service &>>${log_file}
+status_check $?
+
+
+print_head "Reload SystemD"
+systemctl daemon-reload &>>${log_file}
+status_check $?
+
+print_head "Enable ${component} Service"
+systemctl enable ${component} &>>${log_file}
+status_check $?
+
+print_head "Start ${component} Service"
+systemctl start ${component} &>>${log_file}
+status_check $?
+
+Print_head "Copy MongoDB Repo"
+cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
+status_check $?
+
+print_head "Install MongoDB Client"
+yum install mongodb-org-shell -y &>>${log_file}
+status_check $?
+
+print_head "Load Schema"
+mongo --host mongodb.skasadevops.online </app/schema/${component}.js &>>${log_file}
+status_check $?
+}
